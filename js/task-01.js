f714780c3f5c83ab6505da220e3ec58a8919a943
winjher <138558440+winjher@users.noreply.github.com>
2024-05-12 09:54:11 +0800

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,67 +1,16 @@
-//tasks1
-    function Categories(category) {
-      category = document.getElementById("categories").innerHTML;
-      var category = document.querySelector('.categories');
-      var categories = document.getElementById('categories').children.length;
-      console.log("Number of Categories: ", categories);
 
+const categoriesList = document.querySelectorAll('#categories .item');
+console.log(categoriesList);
 
-    }
-    Categories("Categories")
+// 3. Print to log: "Number of categories: " + length of categoriesList
 
+console.log(`Number of categories: ${categoriesList.length}`);
 
-    //console.log("Number of Categories:",)
-    function Animal(myAnimal) {
+// 4. For each category in categoriesList do Steps 5-9
 
-
-      myAnimal = document.getElementById("animal100").innerHTML;
-      var animal100 = document.getElementById("animal100").firstChild.nodeValue;
-      console.log("Category:", animal100);
-      var animals = document.getElementById('list-animal').children.length;
-      console.log("Elements: ", animals);
-      //Lists of animals
-      //const animalList = document.querySelector('.list-animal');
-
-
-      //const animalNodes=animalList.childNodes;
-      //console.log("Elements:",animalNodes)
-    }
-    Animal("Category")
-
-
-    function Food(myFood) {
-
-
-      myFood = document.getElementById("food200").innerHTML;
-      var food200 = document.getElementById("food200").firstChild.nodeValue;
-      console.log("Category:", food200);
-      var foods = document.getElementById('list-products').children.length;
-      console.log("Elements: ", foods);
-      //List of Foods
-      //const foodList = document.querySelector('.list-products');
-
-
-      //const foodNodes=foodList.childNodes;
-      //console.log("Elements:",foodNodes)
-    }
-    Food("Category")
-
-
-    function Technology(myTech) {
-
-
-      myTech = document.getElementById("tech300").innerHTML;
-      var tech300 = document.getElementById("tech300").firstChild.nodeValue;
-      console.log("Category:", tech300);
-      var techs = document.getElementById('list-tech').children.length;
-      console.log("Elements: ", techs);
-
-
-      //List of technology
-      //const techList = document.querySelector('.list-tech');
-
-
-      //const techNodes=techList.childNodes;
-      //console.log("Elements:",techNodes)
-    }
-    Technology("Category")
+categoriesList.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const numberOfElements = category.querySelectorAll('li').length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${numberOfElements}`);
+});
